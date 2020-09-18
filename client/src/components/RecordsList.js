e22875b604c0e340970b5ef5aaec399ecd6e4446
James Fisher <jfisher396@hotmail.com>
2020-09-17 21:11:21 -0700

    mapped return results from API to screen

--- client/src/components/RecordsList.js
+++ client/src/components/RecordsList.js
@@ -4,39 +4,52 @@ import API from '../utils/API';
 export default class RecordsList extends Component {
 
     state = {
-        result: {},
+        results: [],
         search: ""
     }
 
     componentDidMount() {
-        this.searchAlbums("Master of Puppets")
+        this.searchAlbums("goodbye yellow brick road")
     }
 
     searchAlbums = (query) => {
         API.search(query)
           .then((result) => {
-            console.log(result.data.album);
+            // console.log(result.data.album);
+            this.setState({ results: [...result.data.album]})
           })
           .catch((err) => console.log(err));
     }
 
     render() {
+      console.log(this.state.results)
         return (
           <div className="container-fluid">
-            <h1>Records:</h1>
+            <h1>Results:</h1>
             <table className="table">
               <thead>
                 <tr>
+                  <th scope="col">Artwork:</th>
                   <th scope="col">Title:</th>
                   <th scope="col">Artist:</th>
                   <th scope="col">Release year:</th>
-                  <th scope="col">Artwork:</th>
+                  <th scope="col">Genre:</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>{}</td>
-                </tr>
+                {this.state.results.map((record, index) => {
+                  return (
+                    <tr key={record.idAlbum}>
+                      <td>
+                        <img src={record.strAlbumThumb} alt={record.strAlbum} style={{maxWidth: 150}}/>
+                      </td>
+                      <td>{record.strAlbum}</td>
+                      <td>{record.strArtist}</td>
+                      <td>{record.intYearReleased}</td>
+                      <td>{record.strStyle}</td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
